62f1b64be04d8bc2fedbf5615fbfa8ce98200d2e
Anwar Hossain <cseanwar@gmail.com>
2026-05-04 14:24:34 +0200

    changed to auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,17 +6,19 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("pixgen");
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://pixgen-chi-pink.vercel.app/",
+  ],
+
   database: mongodbAdapter(db, {
     client
   }),
   emailAndPassword: {
     enabled: true
   },
-
-  trustedOrigins: [
-    "http://localhost:3000",
-    "https://pixgen-chi-pink.vercel.app/",
-  ],
 
   socialProviders: {
     google: {
