b1260b0b53759a07967341f56cd8e6de18e8cc62
Anwar Hossain <cseanwar@gmail.com>
2026-05-09 12:05:37 +0200

    Added pricing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Banner from "@/components/Banner";
 import TopGenerations from "@/components/TopGenerations";
-import Image from "next/image";
 
 export default function Home() {
   return (
---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pricing = () => {
+    return (
+        <div className='container mx-auto my-10'>
+            <h1 className='font-semibold text-center'>This page is under construction...</h1>
+        </div>
+    );
+};
+
+export default Pricing;
